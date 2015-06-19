c775c051d232072010f3d5a9d7bc732b24cf033e
assafbz <assafbz@gmail.com>
2015-06-20 01:53:31 +0300

    first endpoint with objectify example

---
+++ backend/src/main/java/com/dev/assafbz/betty/backend/Competition.java
@@ -0,0 +1,28 @@
+package com.dev.assafbz.betty.backend;
+
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+/**
+ * The object model for the data we are sending through endpoints
+ */
+@Entity
+public class Competition {
+
+    public Long getId() {
+        return id;
+    }
+
+    @Id
+    Long id;
+
+    public String getCompetitionName() {
+        return competitionName;
+    }
+
+    public void setCompetitionName(String competitionName) {
+        this.competitionName = competitionName;
+    }
+
+    private String competitionName;
+}
